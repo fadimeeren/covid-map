c731d1c9c47243f3b71df573c0fcf46c3c1c2294
fadimeeren <fadime_ern135@outlook.com>
2026-05-05 19:25:37 +0300

    updated

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,7 +1,15 @@
+import { PiVirus } from "react-icons/pi";
+import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
-    <header className="border-b border-white/10 bg-gradient-to-r from-blue-900 via-indigo-900 text-white">
-      Header
+    <header className="border-b border-white/10 bg-linear-to-r from-blue-900 via-indigo-900  to-purple-900 text-white shadow-md backdrop-blur-sm">
+      <div className="container flex items-center justify-between">
+        <Link to="/" className="">
+          <PiVirus className="text-4xl" />
+          <span> Covid-19</span>
+        </Link>
+      </div>
     </header>
   );
 };
