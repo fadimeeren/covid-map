e907a1bb653e84eeed7450fe3efa988eb02fdde8
fadimeeren <fadime_ern135@outlook.com>
2026-05-06 19:37:32 +0300

    Updated

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import Hero from "./hero";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Hero />
+
+      <Statistics />
+    </div>
+  );
 };
 
 export default Home;
