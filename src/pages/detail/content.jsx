6334784e9604e113d81e0cd40fd720bd0aba1819
fadimeeren <fadime_ern135@outlook.com>
2026-07-20 20:45:20 +0300

    Updated

--- src/pages/detail/content.jsx
+++ src/pages/detail/content.jsx
@@ -1,5 +1,25 @@
+import { useSelector } from "react-redux";
+import Card from "./card";
+import ContentLoader from "../../components/loader/content-loader";
+import Error from "../../components/error";
+
 const Content = () => {
-  return <div>Content</div>;
+  const { isLoading, error, data } = useSelector((store) => store);
+
+  // data nesnesini diziye çevir
+  const arr = Object.entries(data || {}).filter(([key]) => key !== "flag");
+
+  return (
+    <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 mt-12">
+      {isLoading ? (
+        <ContentLoader />
+      ) : error ? (
+        <Error message={error} />
+      ) : (
+        arr.map((item, key) => <Card item={item} key={key} />)
+      )}
+    </div>
+  );
 };
 
 export default Content;
